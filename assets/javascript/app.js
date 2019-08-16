065e7a0cd86401ce7a0fa9b8cd4f9ce3a2e993c3
Pat Perry <risingphx1@gmail.com>
2019-08-15 22:15:48 -0400

    css setup for trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+//variable for time in seconds for countdown
+var time = 60;
+
+//function to get remaining time
+function timeRemaining(timeEnd) {
+var seconds = Math.floor( (time/1000) % 60 );
+console.log(seconds);
+};
+
+//initialize timer
+function intializeTimer(timeEnd) {
+  var timer = document.getElementById("#timer");
+}
+
+//variable for setInterval
+
+
+
+});
